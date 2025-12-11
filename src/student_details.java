89f25609c3866b6dbd5a773054c1d48025e6e6ff
1104zortaxnine <241551104@kiit.ac.in>
2025-12-11 16:41:59 +0530

    initial commit FOR LAB1 Q1

---
+++ src/student_details.java
@@ -0,0 +1,13 @@
+// LAB1 QUES 1
+
+public class student_details
+
+{
+    public static void main(String arg[])
+    {
+        System.out.println("NAME: xyz");
+        System.out.println("ROLL NO: 241551100");
+        System.out.println("SECTION: A200");
+        System.out.println("BRANCH: CSE");
+    }
+}
